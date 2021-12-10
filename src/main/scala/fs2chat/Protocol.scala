d077f2454e96b9bcc0e6fcd76c4be791a2094a51
mpilquist <mpilquist@gmail.com>
2021-12-10 08:24:23 -0500

    Use enums

--- src/main/scala/fs2chat/Protocol.scala
+++ src/main/scala/fs2chat/Protocol.scala
@@ -10,11 +10,11 @@ object Protocol {
     utf8_32.as[Username]
 
   /** Base trait for messages sent from the client to the server. */
-  sealed trait ClientCommand
+  enum ClientCommand {
+    case RequestUsername(name: Username)
+    case SendMessage(value: String)
+  }
   object ClientCommand {
-    case class RequestUsername(name: Username) extends ClientCommand
-    case class SendMessage(value: String) extends ClientCommand
-
     val codec: Codec[ClientCommand] = discriminated[ClientCommand]
       .by(uint8)
       .typecase(1, username.as[RequestUsername])
@@ -22,13 +22,13 @@ object Protocol {
   }
 
   /** Base trait for messages sent from the server to the client. */
-  sealed trait ServerCommand
+  enum ServerCommand {
+    case SetUsername(name: Username)
+    case Alert(text: String)
+    case Message(name: Username, text: String)
+    case Disconnect
+  }
   object ServerCommand {
-    case class SetUsername(name: Username) extends ServerCommand
-    case class Alert(text: String) extends ServerCommand
-    case class Message(name: Username, text: String) extends ServerCommand
-    case object Disconnect extends ServerCommand
-
     val codec: Codec[ServerCommand] = discriminated[ServerCommand]
       .by(uint8)
       .typecase(129, username.as[SetUsername])
